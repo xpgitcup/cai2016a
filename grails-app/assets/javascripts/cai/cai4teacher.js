56ce43260b1de769b39d94f5ede3acd2df3cfaf7
LiXiaoping <LiXiaoping@ThinkPad-Lxp>
2016-03-23 15:53:47 +0800

    20160319-完善抽签

--- grails-app/assets/javascripts/cai/cai4teacher.js
+++ grails-app/assets/javascripts/cai/cai4teacher.js
@@ -3,5 +3,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$(function () {
+    //显示当前待选课程
+    listTeaching(1, 10);
+    //我所选的课程
+    listLearning(1, 10);
+    //作业情况
+    listHomework(1, 10);
+    //已完成的作业
+    //当前分组情况
+    listGroup(1,10);
+});
 
+function listGroup(offset, pageSize) {
+    console.info("列出当前的分组情况");
+    
+    $.ajax({
+        url: 'caiTools/queryGroup',
+        data: 'offset=' + offset + '&max=' + pageSize,
+        success: function (data, textStatus) {
+            $('#group').html(data);
+            console.info(data);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.info(XMLHttpRequest);
+            console.info(textStatus);
+            console.info(errorThrown);
+        }
+    });
+}
+
+
+function listHomework(offset, pageSize) {
+    console.info("列出需要完成的作业");
+    
+    $.ajax({
+        url: 'caiTools/queryHomework',
+        data: 'offset=' + offset + '&max=' + pageSize,
+        success: function (data, textStatus) {
+            $('#homework').html(data);
+            console.info(data);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.info(XMLHttpRequest);
+            console.info(textStatus);
+            console.info(errorThrown);
+        }
+    });
+}
+
+function listLearning(offset, pageSize) {
+    console.info("列出我的课程");
+    
+    $.ajax({
+        url: 'caiTools/queryLearning',
+        data: 'offset=' + offset + '&max=' + pageSize,
+        success: function (data, textStatus) {
+            $('#learning').html(data);
+            console.info(data);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.info(XMLHttpRequest);
+            console.info(textStatus);
+            console.info(errorThrown);
+        }
+    });
+}
+
+
+function listTeaching(offset, pageSize) {
+    console.info("列出可选的课程");
+    
+    $.ajax({
+        url: 'caiTools/queryTeaching',
+        data: 'offset=' + offset + '&max=' + pageSize,
+        success: function (data, textStatus) {
+            $('#teaching').html(data);
+            console.info(data);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.info(XMLHttpRequest);
+            console.info(textStatus);
+            console.info(errorThrown);
+        }
+    });
+}
 
